e848285c4a229247e45e6e0cbf797f152b8e21f2
mishary <masharyahmed123@gmail.com>
2025-03-26 21:52:35 +0300

    fix problem of connecting the db multiple times

--- backend/src/auth/auth-service-provider/auth-service-provider.module.ts
+++ backend/src/auth/auth-service-provider/auth-service-provider.module.ts
@@ -3,11 +3,11 @@ import { AuthServiceProviderService } from "./auth-service-provider.service";
 import { AuthServiceProviderController } from "./auth-service-provider.controller";
 import { TwilioModule } from "src/twilio/twilio.module";
 import { AuthModule } from "../auth.module";
-import { DatabaseService } from "src/database/database.service";
+import { DatabaseModule } from "src/database/database.module";
 
 @Module({
-  imports: [TwilioModule, AuthModule],
-  providers: [AuthServiceProviderService, DatabaseService],
+  imports: [TwilioModule, AuthModule, DatabaseModule],
+  providers: [AuthServiceProviderService],
   controllers: [AuthServiceProviderController],
 })
 export class AuthServiceProviderModule {}
